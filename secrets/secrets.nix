944d92e5e0a6ec51f7b1fe8b2ef42780270dadf6
Mario Barros <mariofbarros.fsma@gmail.com>
2026-09-17 21:30:17 -0300

    Rekey searxng secret to add nixbook as a recipient

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,8 +5,8 @@ let
   # too so `agenix -e`/`age -d` work directly from this account, not just
   # at activation time on the target host.
   nixBtw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILI3DZ8EsHZ2WBIgIYvTmDPsCx5FaOZym4LNEbPb5r8Q root@nix-btw";
-  # nixbook = "ssh-ed25519 ..."; # TODO: fill in from /etc/ssh/ssh_host_ed25519_key.pub after deploying there, then re-encrypt with `agenix -r`
+  nixbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMw1SGM7v8Pz7EYRF82d5L4SLNmqUfozPWp5cnwdYLSt root@nixbook";
   mario = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIbtP3jOc+3FufWmuhPL3gMM0KQ8g1Jelm/bzORm7RRL mariofbarros.fsma@gmail.com";
 in {
-  "searxng-secret.env.age".publicKeys = [ nixBtw mario ];
+  "searxng-secret.env.age".publicKeys = [ nixBtw nixbook mario ];
 }
